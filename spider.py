510c52081d542cb67485e5c34124c2b9176605eb
Tanky Woo <wtq1990@gmail.com>
2013-02-27 23:34:23 +0800

    add threadpool

--- spider.py
+++ spider.py
@@ -2,12 +2,18 @@
 # -*- coding: utf-8 -*-
 # tankywoo@2013-02-21
 
+# @todo
+# 增加logging
+# 增加线程池
+
 import urllib2
-import threading
-from Queue import Queue
 from HTMLParser import HTMLParser
 
-url ='http://wiki.wutianqi.com/command/curl.html' 
+from threadpool import ThreadPool
+
+import time
+
+url ='http://www.sina.com.cn/' 
 
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 header = {'User-Agent':user_agent}
@@ -22,28 +28,16 @@ class Parser(HTMLParser):
 		if href:
 			self.urls.extend(href)
 
-class Spider(threading.Thread):
-	def __init__(self, queue, depth=2):
-		threading.Thread.__init__(self)
-		self.queue = queue
+class Spider:
+	def __init__(self, depth=30):
+		self.threadPool = ThreadPool(5)
 		self.depth = depth
-		#self._add_url((1, url))	# root目录是第1层
-	
-	def _add_url(self, url_node):
-		self._fix_url(url_node[1])
-		self.queue.put(url_node)
 
-	def _get_url(self):
-		return self.queue.get()
+	def start(self, url):
+		self.threadPool.initPool()
+		self.threadPool.putTask(self.get_urls, url)
 
-	def _fix_url(self, url):
-		if url.startswith('/'):
-			pass
-		if url.endswith('/'):
-			pass
-
-	def get_urls(self, url_node):
-		_depth, url = url_node		
+	def get_urls(self, url):
 		req = urllib2.Request(url=url, headers=header)
 		try:
 			resp = urllib2.urlopen(req, timeout=10)
@@ -60,35 +54,16 @@ class Spider(threading.Thread):
 		else:
 			print resp.geturl()
 			html = resp.read()
-			if _depth == self.depth:
-				return
 			try:
 				parser = Parser()
 				parser.feed(html)
 			except:
 				print 'unknown error'
-			#print parser.urls
 			for url in parser.urls:
-				self._add_url((_depth+1, url))
+				if url.startswith('http:'):
+					self.threadPool.putTask(self.get_urls, url)
 
-	def run(self):
-		print 'run function'
-		while not self.queue.empty():
-			url_node = self._get_url()
-			self.get_urls(url_node)
-			self.queue.task_done()
-		print 'end'
-
-def Work():
-	queue = Queue()
-	for i in xrange(3):
-		r = Spider(queue)
-		r.setDaemon(True)
-		r.start()
-	queue.put((1, url))
-	queue.join()
 
 if __name__ == '__main__':
-	#myspider = Spider(url)
-	#myspider.run()
-	Work()
+	spider = Spider()
+	spider.start(url)
---
+++ threadpool.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# tankywoo@2013-02-27
+
+from threading import Thread
+from Queue import Queue, Empty
+
+class Worker(Thread):
+	def __init__(self, taskQueue):
+		Thread.__init__(self)
+		self.state = True
+		self.taskQueue = taskQueue
+		self.start()
+
+	def run(self):
+		while True:
+			if not self.state:
+				break
+			try:
+				func, argv = self.taskQueue.get()
+				func(argv)
+				self.taskQueue.task_done()
+			except Empty:
+				continue
+	
+	def stop(self):
+		self.state = False
+
+
+class ThreadPool:
+	def __init__(self, poolSize):
+		self.taskQueue = Queue()
+		self.pool = list()
+		self.poolSize = poolSize
+
+	def initPool(self):
+		for i in xrange(self.poolSize):
+			worker = Worker(self.taskQueue)
+			self.pool.append(worker)
+	
+	def stopPool(self):
+		for _thread in self.pool:
+			_thread.stop()
+			_thread.join()
+	
+	def putTask(self, func, argv):
+		self.taskQueue.put((func, argv))
+
+	def getTask(self):
+		pass
+
+	def taskJoin(self):
+		self.taskQueue.join()
+
